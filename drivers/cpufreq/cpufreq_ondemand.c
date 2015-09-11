4661d68d4910dbe0f1095124e39508158b9f82ed
Bishnu Prasad <bishnuprasad884uu@users.noreply.github.com>
2015-09-11 10:35:27 +0530

    cpufreq: Update cpugovernors

--- drivers/cpufreq/cpufreq_ondemand.c
+++ drivers/cpufreq/cpufreq_ondemand.c
@@ -482,6 +482,8 @@ static int od_init(struct dbs_data *dbs_data)
 	u64 idle_time;
 	int cpu;
 
+	if (cpuboost_enable) cpuboost_enable = false;
+	
 	tuners = kzalloc(sizeof(*tuners), GFP_KERNEL);
 	if (!tuners) {
 		pr_err("%s: kzalloc failed\n", __func__);
